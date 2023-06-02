98fa6cdfabfcdb9c1380a661a5a030550c04ad93
hemant207 <hemantmangukiya93@gmail.com>
2023-06-02 18:07:32 +0100

    feat:updated and added new schema

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,36 @@
+
 import { ServerRespond } from './DataStreamer';
 
+//here we are creating interface with the require schema
 export interface Row {
-  stock: string,
-  top_ask_price: number,
+  price_abc:number,
+  price_def:number,
+  ratio:number,
+  lower_bound: number,
+  upper_bound: number,
+  trigger_alert:number | undefined,
   timestamp: Date,
 }
 
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
+  //we are using data interface Row and assigning values and formulas for the values and returning it
+  static generateRow(serverResponds: ServerRespond[]):Row {
+
+    const price_abc= (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price )/ 2 ;
+    const price_def=(serverResponds[1].top_ask.price + serverResponds[1].top_bid.price )/ 2 ;
+    const ratio=price_abc/price_def;
+    const lower_bound=1 - 0.05;
+    const upper_bound=1 + 0.05;
+  
       return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+        price_abc:price_abc,
+        price_def:price_def,
+        ratio,
+        lower_bound:lower_bound,
+        upper_bound:upper_bound,
+        trigger_alert:(ratio>upper_bound || ratio<lower_bound) ? ratio:undefined,
+        timestamp: serverResponds[0].timestamp > serverResponds[1].timestamp ? serverResponds[0].timestamp : serverResponds[1].timestamp 
+    }
   }
 }
